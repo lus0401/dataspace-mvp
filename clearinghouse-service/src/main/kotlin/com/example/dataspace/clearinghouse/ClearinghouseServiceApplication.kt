a64e1a0e6968d24044232bf67091cd2d9ba20301
lus0101 <lus0401@gmail.com>
2026-01-26 00:10:17 +0900

    feat: clearing house service

---
+++ clearinghouse-service/src/main/kotlin/com/example/dataspace/clearinghouse/ClearinghouseServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dataspace.clearinghouse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClearinghouseServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ClearinghouseServiceApplication>(*args)
+}
---
+++ clearinghouse-service/src/test/kotlin/com/example/dataspace/clearinghouse/ClearinghouseServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dataspace.clearinghouse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClearinghouseServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
